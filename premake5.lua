7a4272e1b772d8b99b6c70f4b539ceb34f00b610
matd3720 <matd3720@mailhost.cs.uleth.ca>
2020-11-23 03:44:31 -0700

    Set up premake

---
+++ premake5.lua
@@ -0,0 +1,92 @@
+workspace "Kokoro"
+  architecture "x64"
+
+  configurations {
+    "Debug",
+    "Release",
+    "Dist"
+  }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Kokoro"
+  location "Kokoro"
+  kind "SharedLib"
+  language "C++"
+
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files {
+    "%{prj.name}/src/**.h",
+    "%{prj.name}/src/**.cpp"
+  }
+
+  includedirs {
+    --"%{prj.name}/vendor/spdlog/include"
+  }
+
+  filter "system:linux"
+    buildoptions { "-std=c++17", "-g"}
+
+    defines {
+      "KO_PLATFORM_LINUX",
+      "KO_BUILD_SO"
+    }
+
+    postbuildcommands {
+      ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+    }
+
+  filter "configurations:Debug"
+    defines "KO_DEBUG"
+    symbols "On"
+
+  filter "configurations:Release"
+    defines "KO_RELEASE"
+    optimize "On"
+
+  filter "configurations:Dist"
+    defines "KO_DIST"
+    symbols "On"
+
+project "Sandbox"
+  location "Sandbox"
+  kind "ConsoleApp"
+  language "C++"
+
+  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files {
+    "%{prj.name}/src/**.h",
+    "%{prj.name}/src/**.cpp"
+  }
+
+  includedirs {
+    --"Kokoro/vendor/spdlog/include"
+    "Kokoro/src"
+  }
+
+  links {
+    "Kokoro"
+  }
+
+  filter "system:linux"
+    buildoptions { "-std=c++17", "-g"}
+
+    defines {
+      "KO_PLATFORM_LINUX"
+    }
+
+  filter "configurations:Debug"
+    defines "KO_DEBUG"
+    symbols "On"
+
+  filter "configurations:Release"
+    defines "KO_RELEASE"
+    optimize "On"
+
+  filter "configurations:Dist"
+    defines "KO_DIST"
+    symbols "On"
